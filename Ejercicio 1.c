bdb502865f823e86b0885bdf81cf58e1e64a6dc3
crwn-up <luidran1@gmail.com>
2025-05-06 14:22:18 -0600

    Create Ejercicio 1.c

---
+++ Ejercicio 1.c
@@ -0,0 +1,16 @@
+//Tarea2
+//Ejercicio 1
+#include <stdio.h>
+int main() {
+float num1, num2;
+printf("ingresa el primer numero:");
+scanf("%f", &num1);
+printf("ingresa el segundo numero:");
+scanf("%f", &num2);
+
+printf("Suma: %.2f\n", num1 + num2);
+printf("Resta: %.2f\n", num1 - num2);
+printf("Multiplicación: %.2f\n", num1 * num2);
+printf("Division: %.2f\n", num1 / num2);
+    return 0;
+}
