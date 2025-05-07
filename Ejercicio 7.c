fd1d45cd35c2855b46d09c3c289b4656317264e4
crwn-up <luidran1@gmail.com>
2025-05-06 21:05:09 -0600

    Create Ejercicio 7.c
    Cuenta con 2 ejercicios, uno considerando dos variables y uno considerando una sola

---
+++ Ejercicio 7.c
@@ -0,0 +1,64 @@
+// Luis Crown
+//Tarea 2
+//Ejercicio 7
+// Ejercicio realizado con una sola variable de distancia, es decir, la distancia de ida se multiplica por dos para considerar ida y vuelta.
+#include <stdio.h>
+
+int main() {
+    int dias;
+    float distanciaIda, distanciaTotal, precio, precioFinal;
+
+    printf("Ingrese la distancia de ida (km): ");
+    scanf("%f", &distanciaIda);
+    printf("Ingrese el número de días de estancia: ");
+    scanf("%d", &dias);
+
+    distanciaTotal = distanciaIda * 2;
+    precio = distanciaTotal * 0.23;
+
+    if (dias > 7 && distanciaTotal > 800) {
+        precioFinal = precio * 0.70; // Aplica 30% de descuento
+    } else {
+        precioFinal = precio;
+    }
+
+    printf("El precio del ticket es: $%.2f\n", precioFinal);
+    return 0;
+}
+
+
+
+
+
+// Luis Crown
+//Tarea 2
+//Ejercicio 7.2
+// Ejercicio realizado con dos variables, es decir, distancia de ida y vuelta si es que cambia para cada una.
+#include <stdio.h>
+
+int main() {
+    int dias;
+    float distanciaIda, distanciaVuelta, distanciaTotal, precio, precioFinal;
+
+    printf("Ingrese la distancia de ida (km): ");
+    scanf("%f", &distanciaIda);
+
+    printf("Ingrese la distancia de vuelta (km): ");
+    scanf("%f", &distanciaVuelta);
+
+    printf("Ingrese el número de días de estancia: ");
+    scanf("%d", &dias);
+
+    distanciaTotal = distanciaIda + distanciaVuelta;
+    precio = distanciaTotal * 0.23;
+
+    if (dias > 7 && distanciaTotal > 800) {
+        precioFinal = precio * 0.70; // Descuento del 30%
+    } else {
+        precioFinal = precio;
+    }
+
+    printf("El precio del ticket es: $%.2f\n", precioFinal);
+
+    return 0;
+}
